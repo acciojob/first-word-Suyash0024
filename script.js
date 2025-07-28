11e6dbf0232d8840f6cbdac1b1a564ba5b5c5417
Suyash Garud <113934597+Suyash0024@users.noreply.github.com>
2025-07-28 12:39:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function firstWord(s) {
   // your code here
+	 str = str.trim();
+
+  
+  const spaceIndex = str.indexOf(" ");
+
+
+  if (spaceIndex === -1) {
+    return str;
+  }
+
+  
+  return str.substring(0, spaceIndex);
+}
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
